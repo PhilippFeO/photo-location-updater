d25972541f61e320aaa0aa8748912bd9c3ddf1a9
Ricardo <Rick45@users.noreply.github.com>
2025-02-12 09:22:44 +0000

    Added Process Dialog for googleTakeOutSplitter

--- googleTakeOutSplitter.py
+++ googleTakeOutSplitter.py
@@ -1,14 +1,13 @@
 import json
 import os
 from datetime import datetime
-
-
-def splitGoogleTakeOut(file_path): 
+from PyQt6.QtWidgets import QProgressDialog
+from PyQt6.QtCore import Qt
+def splitGoogleTakeOut(file_path):
     parsed_data = googleTakeOutSplitter(file_path)
     current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
     output_folder = os.path.join(os.path.dirname(file_path), f'TakeOutOutput_{current_time}')
     save_locations_by_month(parsed_data, output_folder)
-
 
 def googleTakeOutSplitter(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -67,12 +66,19 @@ def googleTakeOutSplitter(file_path):
                     "Longitude": float(longitude)
                 })
     
-    
     return extracted_data
 
 def save_locations_by_month(extracted_data, output_folder):
     locations_by_month = {}
     
+    total_stepsA = extracted_data.__len__()
+    
+    progress_dialogTotals = QProgressDialog("Calculating totals...", "Cancel", 0, total_stepsA)
+    progress_dialogTotals.setWindowTitle("Processing")
+    progress_dialogTotals.setWindowModality(Qt.WindowModality.ApplicationModal)
+    progress_dialogTotals.show()
+
+    stepsForTotals = 0
     for entry in extracted_data:
         date_time = entry['DateTime']
         date_obj = datetime.strptime(date_time, '%Y-%m-%dT%H:%M:%S.%f%z')
@@ -85,7 +91,20 @@ def save_locations_by_month(extracted_data, output_folder):
             locations_by_month[year][month] = []
         
         locations_by_month[year][month].append(entry)
+        stepsForTotals += 1
+        progress_dialogTotals.setValue(stepsForTotals)
+        if progress_dialogTotals.wasCanceled():
+            break
     
+    progress_dialogTotals.close()
+    
+    total_steps = sum(len(months) for months in locations_by_month.values())
+    progress_dialog = QProgressDialog("Saving locations...", "Cancel", 0, total_steps)
+    progress_dialog.setWindowTitle("Processing")
+    progress_dialog.setWindowModality(Qt.WindowModality.ApplicationModal)
+    progress_dialog.show()
+    
+    step = 0
     for year, months in locations_by_month.items():
         year_folder = os.path.join(output_folder, str(year))
         os.makedirs(year_folder, exist_ok=True)
@@ -94,3 +113,12 @@ def save_locations_by_month(extracted_data, output_folder):
             month_file_path = os.path.join(year_folder, f'{month:02d}.json')
             with open(month_file_path, 'w', encoding='utf-8') as month_file:
                 json.dump({"TakeOutSplitterResult": locations}, month_file, ensure_ascii=False, indent=4)
+            
+            step += 1
+            progress_dialog.setValue(step)
+            if progress_dialog.wasCanceled():
+                break
+
+    progress_dialog.close()
+
+
